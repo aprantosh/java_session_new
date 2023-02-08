fcabfa8f1916c83572749e1eabc4dc15b8a76cd3
aprantosh <aprantosh@gmail.com>
2023-02-08 11:15:10 -0600

    dayall new  updatee

---
+++ src/dayPrantosh/Student.java
@@ -0,0 +1,22 @@
+package dayPrantosh;
+
+public class Student {
+
+    private String name;
+    private int rollno;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", rollno=" + rollno +
+                '}';
+    }
+
+    public Student(String abc, int i) {
+    this.name = name;
+    this.rollno = rollno;
+    }
+
+}
+
